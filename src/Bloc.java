43c055986b855621df8e6e3cd7a498c2b413101d
Nawal <nana.miwa@outlook.fr>
2025-01-10 16:35:02 +0100

    faut case et bloc

--- src/Bloc.java
+++ src/Bloc.java
@@ -1,2 +1,26 @@
 public class Bloc {
+    private Case[] cases;
+    public int taille;
+
+    public Bloc(int taille) {
+        this.taille = taille;
+        cases = new Case[this.taille];
+    }
+
+    // Obtenir une case par son index
+    public Case getCase(int index) {
+        if (index < 0 || index >= taille) {
+            throw new IndexOutOfBoundsException("Index invalide : " + index);
+        }
+        return cases[index];
+    }
+
+    // Afficher le contenu du bloc
+    public void afficherBloc() {
+        for (int i = 0; i < taille; i++) {
+            System.out.print((cases[i] != null ? cases[i].getValeur() : "vide") + " ");
+        }
+        System.out.println();
+    }
 }
+
